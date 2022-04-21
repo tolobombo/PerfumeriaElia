8545c187a59558169439fb27e9190adcfb350868
Fernando Reyes <hapoweiser@gmail.com>
2022-04-21 14:54:53 -0700

    se agragaron los textboxs para registrar usuario y su contraseña

--- WindowsFormsApp10/Empleados.Designer.cs
+++ WindowsFormsApp10/Empleados.Designer.cs
@@ -57,6 +57,12 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtID = new System.Windows.Forms.TextBox();
             this.btnRegresar = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).BeginInit();
             this.topPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -76,7 +82,7 @@
             this.dgvEmpleados.Location = new System.Drawing.Point(9, 5);
             this.dgvEmpleados.Name = "dgvEmpleados";
             this.dgvEmpleados.RowHeadersWidth = 51;
-            this.dgvEmpleados.Size = new System.Drawing.Size(533, 296);
+            this.dgvEmpleados.Size = new System.Drawing.Size(533, 349);
             this.dgvEmpleados.TabIndex = 0;
             // 
             // ID
@@ -112,10 +118,10 @@
             this.btnAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnAgregar.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnAgregar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(167)))), ((int)(((byte)(91)))));
-            this.btnAgregar.Location = new System.Drawing.Point(723, 183);
+            this.btnAgregar.Location = new System.Drawing.Point(726, 327);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(86, 27);
-            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.TabIndex = 21;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
@@ -125,7 +131,7 @@
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(167)))), ((int)(((byte)(91)))));
-            this.button2.Location = new System.Drawing.Point(272, 307);
+            this.button2.Location = new System.Drawing.Point(272, 360);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(86, 27);
             this.button2.TabIndex = 2;
@@ -137,7 +143,7 @@
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(167)))), ((int)(((byte)(91)))));
-            this.button3.Location = new System.Drawing.Point(364, 307);
+            this.button3.Location = new System.Drawing.Point(364, 360);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(86, 27);
             this.button3.TabIndex = 3;
@@ -149,7 +155,7 @@
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(167)))), ((int)(((byte)(91)))));
-            this.button4.Location = new System.Drawing.Point(456, 307);
+            this.button4.Location = new System.Drawing.Point(456, 360);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(86, 27);
             this.button4.TabIndex = 4;
@@ -221,13 +227,19 @@
             this.btnSalir.Margin = new System.Windows.Forms.Padding(2);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(38, 24);
-            this.btnSalir.TabIndex = 19;
+            this.btnSalir.TabIndex = 30;
             this.btnSalir.Text = "X";
             this.btnSalir.UseVisualStyleBackColor = true;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.textBox1);
             this.panel1.Controls.Add(this.cmbTipo);
             this.panel1.Controls.Add(this.label7);
             this.panel1.Controls.Add(this.txtCorreo);
@@ -250,7 +262,7 @@
             this.panel1.Location = new System.Drawing.Point(0, 24);
             this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(821, 347);
+            this.panel1.Size = new System.Drawing.Size(821, 394);
             this.panel1.TabIndex = 16;
             // 
             // cmbTipo
@@ -260,21 +272,23 @@
             this.cmbTipo.Items.AddRange(new object[] {
             "Usuario",
             "Administrador"});
-            this.cmbTipo.Location = new System.Drawing.Point(551, 189);
+            this.cmbTipo.Location = new System.Drawing.Point(551, 333);
             this.cmbTipo.Name = "cmbTipo";
             this.cmbTipo.Size = new System.Drawing.Size(121, 21);
-            this.cmbTipo.TabIndex = 18;
+            this.cmbTipo.TabIndex = 20;
+            this.cmbTipo.SelectedIndexChanged += new System.EventHandler(this.cmbTipo_SelectedIndexChanged);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(167)))), ((int)(((byte)(91)))));
-            this.label7.Location = new System.Drawing.Point(548, 172);
+            this.label7.Location = new System.Drawing.Point(548, 316);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(114, 14);
             this.label7.TabIndex = 17;
             this.label7.Text = "Tipo de Usuario:";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
             // 
             // txtCorreo
             // 
@@ -301,25 +315,7 @@
             this.txtApellido2.Size = new System.Drawing.Size(127, 20);
             this.txtApellido2.TabIndex = 14;
             // 
-            // txtApellido1
-            // 
-            this.txtApellido1.Location = new System.Drawing.Point(551, 76);
-            this.txtApellido1.Name = "txtApellido1";
-            this.txtApellido1.Size = new System.Drawing.Size(128, 20);
-            this.txtApellido1.TabIndex = 10;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(167)))), ((int)(((byte)(91)))));
-            this.label3.Location = new System.Drawing.Point(606, 6);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(75, 14);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "Nombre(s):";
-            // 
-            // txtNombre
+            // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
@@ -389,7 +385,7 @@
             this.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnRegresar.Font = new System.Drawing.Font("Bookman Old Style", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRegresar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(167)))), ((int)(((byte)(91)))));
-            this.btnRegresar.Location = new System.Drawing.Point(12, 307);
+            this.btnRegresar.Location = new System.Drawing.Point(12, 360);
             this.btnRegresar.Name = "btnRegresar";
             this.btnRegresar.Size = new System.Drawing.Size(80, 27);
             this.btnRegresar.TabIndex = 5;
@@ -397,12 +393,66 @@
             this.btnRegresar.UseVisualStyleBackColor = true;
             this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
             // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(167)))), ((int)(((byte)(91)))));
+            this.label8.Location = new System.Drawing.Point(548, 166);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 14);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Usuario:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(551, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 20);
+            this.textBox1.TabIndex = 17;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(167)))), ((int)(((byte)(91)))));
+            this.label9.Location = new System.Drawing.Point(548, 215);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 14);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Contraseña:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(551, 232);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(189, 20);
+            this.textBox2.TabIndex = 18;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Bookman Old Style", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(201)))), ((int)(((byte)(167)))), ((int)(((byte)(91)))));
+            this.label10.Location = new System.Drawing.Point(548, 264);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(157, 14);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "Confirmar Contraseña:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(551, 281);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(189, 20);
+            this.textBox3.TabIndex = 19;
+            // 
             // Empleados
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
-            this.ClientSize = new System.Drawing.Size(821, 371);
+            this.ClientSize = new System.Drawing.Size(821, 418);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.topPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
@@ -450,5 +500,11 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
         private System.Windows.Forms.DataGridViewTextBoxColumn Correo;
         private System.Windows.Forms.DataGridViewTextBoxColumn TipoUsuario;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- WindowsFormsApp10/Empleados.cs
+++ WindowsFormsApp10/Empleados.cs
@@ -109,6 +109,16 @@ namespace WindowsFormsApp10
         {
             mouseDown = false;
         }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbTipo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
         ////////////////////////////////////////////////////////////////////
     }
 }
